495a8fb151e603f4a6258956f2384b497dacb5b9
Kholmagorov <112563760+AKholmagorov@users.noreply.github.com>
2024-01-16 16:08:38 +0300

    fix data_converter.dart

--- lib/utils/data_converter.dart
+++ lib/utils/data_converter.dart
@@ -1,33 +1,33 @@
 import 'package:intl/intl.dart';
 
-String ConvertLastSeen(int timestamp) {
-  DateTime lastSeenDate =
-      DateTime.fromMillisecondsSinceEpoch(timestamp * 1000, isUtc: true)
-          .toLocal();
-  DateTime curDate = DateTime.now();
+String ConvertLastSeen(int lastSeenTS) {
+  DateTime lastSeenDate = DateTime.fromMillisecondsSinceEpoch(lastSeenTS * 1000, isUtc: true).toLocal();
+  int dayDifference = (DateTime.now().millisecondsSinceEpoch ~/ 1000 - lastSeenTS) ~/ 86400;
 
-  if (curDate.day == lastSeenDate.day) {
+  if (dayDifference == 0) {
     return 'at ' + DateFormat('HH:mm').format(lastSeenDate);
-  } else if (curDate.day - lastSeenDate.day == 1) {
+  } 
+  else if (dayDifference == 1) {
     return 'yesterday at ' + DateFormat('HH:mm').format(lastSeenDate);
-  } else if (curDate.day - lastSeenDate.day < 7) {
+  } 
+  else if (dayDifference < 7) {
     return DateFormat('E').format(lastSeenDate) +
         ' at ' +
         DateFormat('HH:mm').format(lastSeenDate);
-  } else if (curDate.day - lastSeenDate.day >= 7 &&
-      curDate.year == lastSeenDate.year) {
+  }
+  else if (dayDifference < 365) {
     return DateFormat('MMMd').format(lastSeenDate) +
         ' at ' +
         DateFormat('HH:mm').format(lastSeenDate);
-  } else {
-    return DateFormat('yMMMd').format(lastSeenDate);
+  } 
+  else {
+    return DateFormat('yMMMd').format(lastSeenDate) +
+        ' at ' +
+        DateFormat('HH:mm').format(lastSeenDate);
   }
 }
 
 String ConvertMsgDate(int timestamp) {
-  DateTime msgDate =
-      DateTime.fromMillisecondsSinceEpoch(timestamp * 1000, isUtc: true)
-          .toLocal();
-
+  DateTime msgDate = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000, isUtc: true).toLocal();
   return DateFormat('HH:mm').format(msgDate);
 }
